edff196961d03d3dcf8a416244a454aa92f19bf1
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2022-03-06 02:13:26 +0300

    collapsing and showing cards

--- src/1.5/cards.js
+++ src/1.5/cards.js
@@ -1,28 +1,39 @@
-// const cardsArea = document.querySelector('.cards-area');
+const collapseButton = document.querySelector('.collapse-button');
 
-// let cardsHolder = '<h1>hello</h1>';
-
-async function getCards() {
+async function renderCards() {
   let cardsResponse = await fetch('./brands.json');
   let cards = await cardsResponse.json();
 
-  let cardsHolder = '';
+  let cardsLimit;
 
-  for (let i = 0; i < cards.length; i++) {
-    console.log(cards[i]);
-    cardsHolder += `<img src="../../assets/brands/${cards[i].file}">`;
+  if (collapseButton.classList.contains('content-is-collapsed')) {
+    if (window.innerWidth >= 768 && window.innerWidth < 1120) {
+      cardsLimit = 6;
+    } else if (window.innerWidth >= 1120) {
+      cardsLimit = 8;
+    }
+  } else {
+    cardsLimit = cards.length;
   }
 
-  const cardsArea = document.querySelector('.cards-area');
+  let cardsHolder = '';
+
+  for (let i = 0; i < cardsLimit; i++) {
+    cardsHolder += `<div class="brands__card-item"><img src="../../assets/brands/${cards[i].file}"><div class="check-partner-button"></div></div>`;
+  }
+
+  const cardsArea = document.querySelector('.brands');
   cardsArea.innerHTML = cardsHolder;
 }
 
-getCards();
+renderCards();
 
-// console.log('fetched cards: ', fetchedCards);
-
-// for (let i = 0; i < 9; i++) {
-//   cardsHolder += '<h1>hello</h1>';
-// }
-
-// cardsArea.innerHTML = cardsHolder;
+collapseButton.addEventListener('click', (e) => {
+  if (collapseButton.classList.contains('content-is-collapsed')) {
+    collapseButton.innerHTML = 'Скрыть';
+  } else {
+    collapseButton.innerHTML = 'Показать все';
+  }
+  collapseButton.classList.toggle('content-is-collapsed');
+  renderCards();
+});
