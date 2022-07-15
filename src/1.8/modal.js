fd5a2eaa1669438b9b3b80e9c8c19586eb034834
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2022-07-15 14:54:16 +0700

    popups

--- src/1.8/modal.js
+++ src/1.8/modal.js
@@ -1,11 +1,17 @@
-let closePopupBtn = document.querySelector('.close-popup-button');
+// phone button
+
+let closePopupButtons = document.querySelectorAll('.close-popup-button');
 let callBackPopup = document.querySelector('.popup-call-back');
+let feedbackPopup = document.querySelector('.popup-feedback');
 let phoneButtons = document.querySelectorAll('.icon-phone');
+let messageButtons = document.querySelectorAll('.icon-message');
 
-console.log('phoneButtons', phoneButtons);
-
-closePopupBtn.addEventListener('click', (e) => {
-  callBackPopup.style.display = 'none';
+closePopupButtons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    feedbackPopup.style.display = 'none';
+    callBackPopup.style.display = 'none';
+    e.preventDefault();
+  });
 });
 
 phoneButtons.forEach((btn) => {
@@ -14,6 +20,8 @@ phoneButtons.forEach((btn) => {
   });
 });
 
-// phoneButtons.addEventListener('click', (e) => {
-//   callBackPopup.style.display = 'block';
-// });
+messageButtons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    feedbackPopup.style.display = 'block';
+  });
+});
