f20a9e1b377a373529cdb4c2e12963e14139f655
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2022-07-18 22:26:34 +0700

    disabling and enabling scroll

--- src/1.6/modal.js
+++ src/1.6/modal.js
@@ -9,6 +9,14 @@ let messageButtons = document.querySelectorAll('.icon-message');
 let burgerButtons = document.querySelectorAll('.icon-burger');
 let sidebar = document.querySelector('.sidebar');
 
+let disableScroll = () => {
+  document.body.classList.add('disable-scroll');
+};
+
+let enableScroll = () => {
+  document.body.classList.remove('disable-scroll');
+};
+
 closePopupButtons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     feedbackPopup.style.display = 'none';
@@ -19,6 +27,8 @@ closePopupButtons.forEach((btn) => {
       sidebar.style.position = 'fixed';
     }
 
+    enableScroll();
+
     let pageBackground = document.querySelector('.page');
     e.preventDefault();
   });
@@ -27,18 +37,22 @@ closePopupButtons.forEach((btn) => {
 phoneButtons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     callBackPopup.style.display = 'block';
+    document.body.classList.add('disable-scroll');
+    disableScroll();
   });
 });
 
 messageButtons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     feedbackPopup.style.display = 'block';
+    disableScroll();
   });
 });
 
 burgerButtons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    sidebar.style.display = 'block';
+    sidebar.style.display = 'flex';
+    disableScroll();
   });
 });
 
@@ -47,6 +61,7 @@ let popupCallbackBackground = document.querySelector('.popup-call-back');
 popupCallbackBackground.addEventListener('click', (e) => {
   if (e.target.classList.contains('popup-call-back')) {
     callBackPopup.style.display = 'none';
+    enableScroll();
   }
 });
 
@@ -55,5 +70,6 @@ let popupFeedbackBackground = document.querySelector('.popup-feedback');
 popupFeedbackBackground.addEventListener('click', (e) => {
   if (e.target.classList.contains('popup-feedback')) {
     feedbackPopup.style.display = 'none';
+    enableScroll();
   }
 });
