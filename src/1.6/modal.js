6ae6338ab7a2f4bfe79b72d5380eb8d878fac999
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2022-07-18 19:16:22 +0700

    modals

--- src/1.6/modal.js
+++ src/1.6/modal.js
@@ -1,15 +1,25 @@
 // phone button
 
-let closePopupButtons = document.querySelectorAll('.close-popup-button');
+let closePopupButtons = document.querySelectorAll('.icon-close');
 let callBackPopup = document.querySelector('.popup-call-back');
 let feedbackPopup = document.querySelector('.popup-feedback');
 let phoneButtons = document.querySelectorAll('.icon-phone');
 let messageButtons = document.querySelectorAll('.icon-message');
 
+let burgerButtons = document.querySelectorAll('.icon-burger');
+let sidebar = document.querySelector('.sidebar');
+
 closePopupButtons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     feedbackPopup.style.display = 'none';
     callBackPopup.style.display = 'none';
+
+    if (window.innerWidth <= 1420) {
+      sidebar.style.display = 'none';
+      sidebar.style.position = 'fixed';
+    }
+
+    let pageBackground = document.querySelector('.page');
     e.preventDefault();
   });
 });
@@ -25,3 +35,19 @@ messageButtons.forEach((btn) => {
     feedbackPopup.style.display = 'block';
   });
 });
+
+burgerButtons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    sidebar.style.display = 'block';
+  });
+});
+
+let popupCallbackBackground = document.querySelector('.popup-call-back');
+
+popupCallbackBackground.addEventListener('click', (e) => {
+  console.log('Клик по области попапа');
+});
+
+// window.onclick = (e) => {
+//   if (e.trarget = )
+// };
